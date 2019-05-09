d62fce06333defc8c7552e66e23f2fafbf702226
Gahon <Gahon1995@gmail.com>
2019-05-09 17:25:58 +0800

    登录后个人信息展示

--- web/app.py
+++ web/app.py
@@ -4,14 +4,15 @@ from service.user_service import UserService
 
 from main import init_connect
 
-init_connect()
-
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return render_template('login.html')
+def index(is_login=False, user=None):
+    if not is_login:
+        return render_template('login.html')
+
+    return render_template('index.html', user=user)
 
 
 @app.route('/login', methods=['POST', 'GET'])
@@ -19,14 +20,36 @@ def login():
     username = request.form.get('username')
     password = request.form.get('password')
     remember = request.form.get('remember')
-    success = False
+    user = None
     if username and password:
-        success = UserService.login(username, password)
-    if success:
-        return 'welcome\n' + username + '\n' + password
+        user = UserService.login(username, password)
+    if user:
+        return index(True, user)
     else:
         return render_template('login.html', message="登录失败")
 
 
+@app.route('/admin')
+def admin(is_login=False):
+    if not is_login:
+        return render_template('admin/login.html')
+    pass
+
+
+@app.route('/admin/login', methods=['post', 'get'])
+def admin_login():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    success = False
+    if username == 'admin' and password:
+        success = UserService.login(username, password)
+    if success:
+        return 'welcome\n' + username + '\n' + password
+    else:
+        return render_template('admin/login.html', message="登录失败")
+    pass
+
+
 if __name__ == '__main__':
+    init_connect()
     app.run()
